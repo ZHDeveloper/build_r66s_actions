9b241611dd8b97db8d69ca866c5527876fa79dbd
ZhiHua <shenzhihua@zuowenzhitiao.com>
2025-06-19 14:53:35 +0800

    使用配置的主题包；

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -16,10 +16,6 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # 修改默认主机名
 sed -i 's/OpenWrt/R66S/g' package/base-files/files/bin/config_generate
 
-# 添加主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-
 # 修改版本号
 sed -i "s/OpenWrt /R66S Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
