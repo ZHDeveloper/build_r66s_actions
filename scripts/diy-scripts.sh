8ed11b5c42b7825e199bb810dc623c3df9c908e6
ZhiHua <shenzhihua@zuowenzhitiao.com>
2025-06-20 21:01:41 +0800

    修改默认主题；

--- scripts/diy-scripts.sh
+++ scripts/diy-scripts.sh
@@ -70,8 +70,8 @@ git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/luci-app
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git_sparse_clone main https://github.com/haiibo/packages luci-theme-opentomcat
 
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
