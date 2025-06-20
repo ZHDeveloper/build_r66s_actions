5e8b3f14d6fb5489272efd28acc1cea64b920fd8
ZhiHua <shenzhihua@zuowenzhitiao.com>
2025-06-20 15:51:45 +0800

    添加编译缓存；

--- scripts/diy-scripts.sh
+++ scripts/diy-scripts.sh
@@ -16,14 +16,19 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # 修改默认主机名
 sed -i 's/OpenWrt/R66S/g' package/base-files/files/bin/config_generate
 
-# 修改版本号
-sed -i "s/OpenWrt /R66S Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+# 修改版本号 (仅在 LEDE 源码中存在)
+if [ -f "package/lean/default-settings/files/zzz-default-settings" ]; then
+    sed -i "s/OpenWrt /R66S Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+fi
 
 # 修改时区
 sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/system.@system\[0\].timezone/d" package/base-files/files/bin/config_generate
 echo "        set system.@system[0].timezone='CST-8'" >> package/base-files/files/bin/config_generate
 echo "        set system.@system[0].zonename='Asia/Shanghai'" >> package/base-files/files/bin/config_generate
+
+# TTYD 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 添加自定义文件
 if [ -d "files" ]; then
