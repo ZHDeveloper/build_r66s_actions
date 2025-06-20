337a561dd8768979c7a0cf811fc16315f1aa66f5
ZhiHua <shenzhihua@zuowenzhitiao.com>
2025-06-20 20:38:52 +0800

    修复编译报错；

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mkdir -p files/usr/bin/AdGuardHome
+# 创建目录结构
+mkdir -p files/usr/bin
+
+# 下载 AdGuardHome 核心文件
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
-chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+
+# 直接下载到 /usr/bin/AdGuardHome 文件（不是目录）
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
+chmod +x files/usr/bin/AdGuardHome
