8611cc585f5a107fb5df64c68ea04ba48564e7c3
zhihua <zhcoder2011@gmail.com>
2025-06-21 17:06:21 +0800

    调整脚本；

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# 创建目录结构
-mkdir -p files/usr/bin
-
-# 下载 AdGuardHome 核心文件
+mkdir -p files/usr/bin/AdGuardHome
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
-
-# 直接下载到 /usr/bin/AdGuardHome 文件（不是目录）
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
-chmod +x files/usr/bin/AdGuardHome
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+chmod +x files/usr/bin/AdGuardHome/AdGuardHome
